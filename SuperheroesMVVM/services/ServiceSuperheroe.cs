7efd4695bdcad2dceb0f851f1fd9d20f6da7a8a2
AitanaCaro <aitana.caro@iesdoctorbalmis.com>
2023-01-09 12:06:05 +0100

    Agregar archivos de proyecto.

---
+++ SuperheroesMVVM/services/ServiceSuperheroe.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperheroesMVVM.services
+{
+    class ServiceSuperheroe
+    {
+        public ObservableCollection<Superheroe> getSuperheroes()
+        {
+            return Superheroe.GetSamples();
+        }
+    }
+}
